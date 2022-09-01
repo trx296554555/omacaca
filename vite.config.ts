4860554c0a3aca74e05e9b0b5f22c6b1eb086c69
Tang <296554555@qq.com>
2022-09-01 17:17:36 +0800

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
-		alias: {},
+		alias: {
+			'@': resolve(__dirname, 'src'),
+		},
 	},
 	plugins: [
 		vue(),
